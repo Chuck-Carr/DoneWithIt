2a143dc01031748f67f248dda33b240334c3095f
chuck-carr <chuck.carr4@yahoo.com>
2025-01-24 14:53:42 -0500

    added files

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-
-  return (
-    <WelcomeScreen />
-  )
+  return <WelcomeScreen />;
 }
-
-
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,36 +1,36 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { ImageBackground } from 'react-native-web'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { ImageBackground } from "react-native-web";
 
 export default function WelcomeScreen() {
   return (
-    <View>
-      <View 
-      style={styles.background}
-      >
-        <Image source={require("../../app/assets/")}/>
-        <View style={styles.loginButton}></View>
-        <View style={styles.registerButton}></View>
-      </View>
-    </View>
-  )
+    <ImageBackground source={require("../assets/background.jpg")}>
+      <Image style={styles.logo} source={require("../assets/logo-red.jpg")} />
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        ImageBackground: require("../assets/backgroundimage.jpg"),
-        flex: 1,
-        justifyContent: "flex-end",
-
-    },
-    loginButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: '#fc5c65'
-    },
-    registerButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: '#4ecdc4'
-    }
-})
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 50,
+  },
+});
